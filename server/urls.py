888b4b5178c43c8fd7d07bfcca348d142c91cd31
Utsav Chatterjee <88131195+Utsavch189@users.noreply.github.com>
2022-10-13 11:56:35 +0530

    Update urls.py

--- server/urls.py
+++ server/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from django.contrib.auth.hashers import make_password,check_password
-from Auth.models import OTP
 from datetime import date
 
 
@@ -36,10 +35,3 @@ urlpatterns = [
     path('retailer/',include('Retailer.urls'))
 ]
 
-if OTP.objects.exists():
-    obj=OTP.objects.all()
-    for i in range(0,obj.count()):
-        created_date=obj.values('created_at_date')[i]['created_at_date'].strftime('%m/%d/%Y')
-        if(date.today().strftime('%m/%d/%Y')!=created_date):
-            obj.delete()
-#print(make_password('admin'))
