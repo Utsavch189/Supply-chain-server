74695c2ba05067dc988cdccb2983997ba98abc76
Utsav <utsavpokemon9000chatterjee@gmail.com>
2022-10-13 11:49:53 +0530

    fsgh

--- Auth/migrations/0001_initial.py
+++ Auth/migrations/0001_initial.py
@@ -13,22 +13,20 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='ApprovedUsers',
+            name='OTP',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(blank=True, max_length=40, null=True)),
-                ('phone', models.CharField(blank=True, max_length=15, null=True)),
-                ('email', models.CharField(blank=True, max_length=46, null=True)),
-                ('password', models.CharField(blank=True, max_length=256, null=True)),
-                ('gender', models.CharField(blank=True, max_length=6, null=True)),
-                ('whatsapp_no', models.CharField(blank=True, max_length=15, null=True)),
-                ('role', models.CharField(blank=True, max_length=15, null=True)),
-                ('id_no', models.CharField(blank=True, max_length=25, null=True)),
-                ('approved_at', models.DateField(verbose_name=datetime.date(2022, 10, 13))),
+                ('target', models.CharField(blank=True, max_length=46, null=True)),
+                ('otp', models.CharField(blank=True, max_length=4, null=True)),
+                ('typed', models.CharField(blank=True, max_length=30, null=True)),
+                ('tried', models.IntegerField(blank=True, default=0, null=True)),
+                ('blocked', models.BooleanField(default=False)),
+                ('created_at_date', models.DateField(verbose_name=datetime.date(2022, 10, 13))),
+                ('created_at_time', models.TimeField(verbose_name=datetime.time(11, 46, 3, 71824))),
             ],
         ),
         migrations.CreateModel(
-            name='DeletedUsers',
+            name='Register',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(blank=True, max_length=40, null=True)),
@@ -39,7 +37,7 @@ class Migration(migrations.Migration):
                 ('whatsapp_no', models.CharField(blank=True, max_length=15, null=True)),
                 ('role', models.CharField(blank=True, max_length=15, null=True)),
                 ('id_no', models.CharField(blank=True, max_length=25, null=True)),
-                ('deleted_at', models.DateField(verbose_name=datetime.date(2022, 10, 13))),
+                ('created_at', models.DateField(verbose_name=datetime.date(2022, 10, 13))),
             ],
         ),
     ]
